f2710d616c372b12b9df0e9bf299a82abee69a85
Andrew <111512388+aasnowflake@users.noreply.github.com>
2022-08-19 11:03:17 -0500

    Update streamlit_app.py
    added structure for chooser

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,23 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:    
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
-#create input box to select a fruit
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+except URLError as e:
+       streamlit.error()
 
 
-# normalizes fruityvice response data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# puts normalized data into a table for display
-streamlit.dataframe(fruityvice_normalized)
+
+
+
+
 
 #dont run anything past here while we troubleshoot
 streamlit.stop()
